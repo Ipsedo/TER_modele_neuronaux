827e4081b3f256d897757a78abec3d0a43e9894f
samuel <berriens@gmail.com>
2018-03-28 16:18:48 +0200

    add conv modele -> flop

--- scripts/model.py
+++ scripts/model.py
@@ -18,3 +18,24 @@ class EmbeddingBagModel(nn.Module):
 		#out = self.linear2(out)
 		#out = self.sig2(out)
 		return out
+
+class ConvModel(nn.Module):
+
+	def __init__(self, vocab_size, embedding_size, padding, sent_length):
+		super(ConvModel, self).__init__()
+		self.embeds_dim = embedding_size
+		self.sent_length = sent_length
+		self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=padding)
+		self.conv1D_1 = nn.Conv1d(embedding_size, embedding_size, 3, stride=1, padding=padding)
+		self.sig1 = nn.Sigmoid()
+		self.linear1 = nn.Linear(embedding_size, 1)
+		self.sig2 = nn.Sigmoid()
+
+	def forward(self, inputs):
+		out = self.embedding(inputs)
+		out = out.view((-1, self.embeds_dim, self.sent_length))
+		out = self.conv1D_1(out)
+		#out = self.sig1(out)
+		out = out.sum(dim = 2)
+		out = self.linear1(out)
+		return self.sig2(out)
